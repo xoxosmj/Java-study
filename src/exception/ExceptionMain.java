fef0f3693fc4ae5bcdcd1c900311e91105a548c1
Semook Jung <xoxosmj@gmail.com>
2024-07-19 19:35:44 +0900

    1

--- src/exception/ExceptionMain.java
+++ src/exception/ExceptionMain.java
@@ -4,28 +4,40 @@ import java.util.Scanner;
 
 public class ExceptionMain {
 
-    public static void main(String[] args) {
-        if (args.length != 0) System.out.println("args[0] = " + args[0]);
-        System.out.println();
+	public static void main(String[] args) {
+		if(args.length != 0) System.out.println("args[0] = " + args[0]);
+		System.out.println();
+		
+		try {
+			int num1 = Integer.parseInt(args[0]);
+			
+			Scanner scan = new Scanner(System.in);
+			System.out.print("숫자 입력 : ");
+			int num2 = scan.nextInt();
+					
+			System.out.println(num1 + " / " + num2 + " = " + (num1/num2));
+		
+		}catch(NumberFormatException e) {
+			System.out.println("숫자 형식으로 넣으세요");
+			e.printStackTrace();
+		}catch(ArithmeticException e) {
+			System.out.println("0으로 나눌수 없습니다.");
+			e.printStackTrace();
+		}finally{
+			System.out.println("error가 있건 없건 무조건 실행!!");
+		}
 
-        try {
-            int num1 = Integer.parseInt(args[0]);
+	}
 
-            Scanner sc = new Scanner(System.in);
-            System.out.print("숫자 입력 : ");
-            int num2 = sc.nextInt();
-
-            System.out.println(num1 + " / " + num2 + " = " + num1 / num2);
-
-        } catch (NumberFormatException e) {
-            System.out.println("숫자 형식으로 입력하세요");
-            e.printStackTrace(); // 빨간// 색 에러메세지도 동봉
-        }catch (ArithmeticException e){
-            System.out.println("0으로 나눌수 없습니다");
-            e.printStackTrace();
-        }finally {
-            System.out.println("err가 있건 없건 무조건 실행!!!!!");
-        }
-
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
